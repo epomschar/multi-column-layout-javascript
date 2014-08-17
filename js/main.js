4eed72dff3a3e8eab4e048f4e56dfbea49a5cc5d
epomschar <evzenpomschar@gmail.com>
2014-08-17 13:12:41 +0000

    final

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
-
+function renderGrid(){
+    var blocks = document.getElementById('grid_container').children,
+        pad = 10,
+        cols = 4,
+        newLeft,
+        newTop;
+    for(var i = 1; i < blocks.length; i++){
+        if(i % cols == 0){
+            newTop = blocks[i - cols].offsetTop + blocks[i - cols].offsetHeight + pad;
+            blocks[i].style.top = newTop + "px";            
+        } else {
+            if(blocks[i - cols]){
+                newTop = blocks[i - cols].offsetTop + blocks[i - cols].offsetHeight + pad;
+                blocks[i].style.top = newTop + "px";             
+            }
+            
+            newLeft = blocks[i - 1].offsetLeft + blocks[i - 1].offsetWidth + pad;
+            blocks[i].style.left = newLeft + "px";
+        }
+    }
+    
+}
+window.addEventListener('load', renderGrid, false);
+window.addEventListener('resize', renderGrid, false);
